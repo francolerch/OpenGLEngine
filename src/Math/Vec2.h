971b20a6069c561826c6fb51d2ae5902ef4a0b72
Franco Lerch <franco.lerch@gmail.com>
2020-01-18 01:09:08 -0300

    added Point, vec2, vec3, vec4, math.h

---
+++ src/Math/Vec2.h
@@ -0,0 +1,106 @@
+#pragma once
+#include "Math.h"
+#include "Point.h"
+
+template<typename T>
+struct _Vec2
+{
+	_Point<T> x;
+	_Point<T> y;
+
+	_Vec2() = delete;
+	_Vec2(T x, T y) : x(x), y(y) {};
+	virtual ~_Vec2() {};
+	template <typename T2>
+	explicit operator _Vec2<T2>() const
+	{
+		return { (T2)x,(T2)y };
+	}
+	T LengthSq() const
+	{
+		return Sq(*this);
+	}
+	T Length() const
+	{
+		return sqrt(LengthSq());
+	}
+	_Vec2& Normalize()
+	{
+		const T length = Length();
+		x /= length;
+		y /= length;
+		return *this;
+	}
+	_Vec2 GetNormalized() const
+	{
+		_Vec2 norm = *this;
+		norm.Normalize();
+		return norm;
+	}
+	_Vec2 operator-() const
+	{
+		return _Vec2(-x, -y);
+	}
+	_Vec2& operator=(const _Vec2 & v)
+	{
+		x = v.x;
+		y = v.y;
+		return *this;
+	}
+	_Vec2& operator+=(const _Vec2 & v)
+	{
+		x += v.x;
+		y += v.y;
+		return *this;
+	}
+	_Vec2& operator-=(const _Vec2 & v)
+	{
+		x -= v.x;
+		y -= v.y;
+		return *this;
+	}
+	T operator*(const _Vec2 & v) const
+	{
+		return x * v.x + y * v.y;
+	}
+	_Vec2 operator+(const _Vec2 & v) const
+	{
+		return _Vec2(*this) += v;
+	}
+	_Vec2 operator-(const _Vec2 & v) const
+	{
+		return _Vec2(*this) -= v;
+	}
+	_Vec2& operator*=(const T & v)
+	{
+		x *= v;
+		y *= v;
+		return *this;
+	}
+	_Vec2 operator*(const T & v) const
+	{
+		return _Vec2(*this) *= v;
+	}
+	_Vec2& operator/=(const T & v)
+	{
+		x /= v;
+		y /= v;
+		return *this;
+	}
+	_Vec2 operator/(const T & v) const
+	{
+		return _Vec2(*this) /= v;
+	}
+	bool operator==(const _Vec2 & v) const
+	{
+		return x == v.x && y == v.y;
+	}
+	bool operator!=(const _Vec2 & v) const
+	{
+		return !(*this == v);
+	}
+};
+
+typedef _Vec2<float> Vec2;
+typedef _Vec2<double> Vec2d;
+typedef _Vec2<int> Vec2i;
---
+++ src/Math/Vec3.h
@@ -0,0 +1,111 @@
+#pragma once
+#include "Math.h"
+#include "Vec2.h"
+
+template<typename T>
+struct _Vec3 : public _Vec2<T>
+{
+	_Point<T> z;
+
+	_Vec3() = delete;
+	_Vec3(T x, T y, T z) : x(x), y(y), z(z) {};
+	virtual ~_Vec3() {};
+	template <typename T2>
+	explicit operator _Vec3<T2>() const
+	{
+		return{ (T2)x,(T2)y };
+	}
+	T LengthSq() const
+	{
+		return Sq(*this);
+	}
+	T Length() const
+	{
+		return sqrt(LengthSq());
+	}
+	_Vec3& Normalize()
+	{
+		const T length = Length();
+		x /= length;
+		y /= length;
+		z /= length;
+		return *this;
+	}
+	_Vec3 GetNormalized() const
+	{
+		_Vec3 norm = *this;
+		norm.Normalize();
+		return norm;
+	}
+	_Vec3 operator-() const
+	{
+		return _Vec3(-x, -y, -z);
+	}
+	_Vec3& operator=(const _Vec3& v)
+	{
+		x = v.x;
+		y = v.y;
+		z = v.z;
+		return *this;
+	}
+	_Vec3& operator+=(const _Vec3& v)
+	{
+		x += v.x;
+		y += v.y;
+		z += v.z;
+		return *this;
+	}
+	_Vec3& operator-=(const _Vec3& v)
+	{
+		x -= v.x;
+		y -= v.y;
+		z -= v.z;
+		return *this;
+	}
+	T operator*(const _Vec3& v) const
+	{
+		return x * v.x + y * v.y + z * v.z;
+	}
+	_Vec3 operator+(const _Vec3& v) const
+	{
+		return _Vec3(*this) += v;
+	}
+	_Vec3 operator-(const _Vec3& v) const
+	{
+		return _Vec3(*this) -= v;
+	}
+	_Vec3& operator*=(const T& v)
+	{
+		x *= v;
+		y *= v;
+		z *= v;
+		return *this;
+	}
+	_Vec3 operator*(const T& v) const
+	{
+		return _Vec3(*this) *= v;
+	}
+	_Vec3& operator/=(const T& v)
+	{
+		x /= v;
+		y /= v;
+		z /= v;
+		return *this;
+	}
+	_Vec3 operator/(const T& v) const
+	{
+		return _Vec3(*this) /= v;
+	}
+	bool operator==(const _Vec3& v) const
+	{
+		return x == v.x && y == v.y && z == v.z;
+	}
+	bool operator!=(const _Vec3& v) const
+	{
+		return !(*this == v);
+	}
+};
+
+typedef _Vec3<float> Vec3;
+typedef _Vec3<double> Vec3d;
+typedef _Vec3<int> Vec3i;
