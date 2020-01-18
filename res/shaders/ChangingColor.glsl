728d2456534e9617ed1cd7ea63b4e952c30c8877
Franco Lerch <franco.lerch@gmail.com>
2020-01-18 19:37:04 -0300

    added camera

--- res/shaders/ChangingColor.glsl
+++ res/shaders/ChangingColor.glsl
@@ -6,10 +6,7 @@ layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTexCoord;
 
 out vec3 ourColor; 
-out vec4 ourPosition;
 out vec2 texCoord;
-
-uniform float u_Time;
 uniform mat4 u_Transform;
 uniform mat4 model;
 uniform mat4 view;
@@ -17,8 +14,6 @@ uniform mat4 projection;
 
 void main()
 {
-    float s = sin(u_Time) * 0.5 + 1;
-    ourPosition = vec4(aPos.xyz * s, 1.0);
     gl_Position =  projection * view * model * u_Transform * vec4(aPos, 1.0);
     ourColor = aColor;
     texCoord = aTexCoord;
@@ -28,7 +23,6 @@ void main()
 #version 330 core
 
 in vec3 ourColor;
-in vec4 ourPosition;
 in vec2 texCoord;
 
 out vec4 FragColor;
@@ -36,7 +30,6 @@ out vec4 FragColor;
 uniform sampler2D ourTexture;
 uniform sampler2D texture2;
 uniform vec4 u_Color;
-uniform float u_Opacity;
 
 void main()
 {
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,11 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
 float vertices[] = {
     // positions            // colors           // texture coords
     -1.00f, -1.00f, -.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
@@ -52,17 +57,29 @@ static void error_callback(int error, const char* description)
     std::cerr << "(" << error << ")" << "Error: %s\n" << description << std::endl;
 }
 
-float opacity = 0.0f;
+const float WIDTH = 800.f;
+const float HEIGHT = 600.f;
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 4.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 640.0f / 2.0;
+float lastY = 480.0 / 2.0;
+float fov = 45.0f;
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
-    else if (key == GLFW_KEY_UP && action == GLFW_PRESS)
-        opacity += 0.5f;
-    else if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
-        opacity -= 0.5f;
 }
+
+
  
 int main(void)
 {
@@ -75,8 +92,9 @@ int main(void)
  
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
- 
-    window = glfwCreateWindow(640, 480, "OpenGLEngine", NULL, NULL);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGLEngine", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -91,6 +109,12 @@ int main(void)
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    glEnable(GL_DEPTH_TEST);
     glfwSwapInterval(1);
 
     Shader shader("res/shaders/ChangingColor.glsl");
@@ -125,27 +149,35 @@ int main(void)
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-    glm::mat4 view = glm::mat4(1.0f);
+    //glm::mat4 view = glm::mat4(1.0f);
     // note that we're translating the scene in the reverse direction of where we want to move
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -4.0f));
+    //view = glm::translate(view, glm::vec3(0.0f, 0.0f, -4.0f));
 
     glm::mat4 proj;
-    proj = glm::perspective(glm::radians(65.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    proj = glm::perspective(glm::radians(fov), WIDTH / HEIGHT, 0.1f, 100.0f);
     //trans *= scale;
-    float startTime = glfwGetTime();
-    float elapsedTime = glfwGetTime();
-    //glfwSetTime(0.0);
 
-    glEnable(GL_DEPTH_TEST);
-
-   
     while (!glfwWindowShouldClose(window))
     {
+        // per-frame time logic
+        // ---------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // ---------------------
+        processInput(window);
+
+        // render
+        // ---------------------
         glm::mat4 trans = glm::mat4(1.0f);
         trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
         trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.5f, 0.7f, 1.0f));
 
-        elapsedTime = glfwGetTime();
+
+        glm::mat4 view = glm::lookAt(cameraPos, cameraFront + cameraPos, cameraUp);
+
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -154,7 +186,7 @@ int main(void)
 
 
 
-        float greenValue = (sin(startTime) / 2.0f) + 0.5f;
+        float greenValue = 1.f;//(sin(startTime) / 2.0f) + 0.5f;
 
 
 
@@ -165,8 +197,6 @@ int main(void)
         shader.SetUniformMat4f("view", view);
         shader.SetUniformMat4f("projection", proj);
 
-        shader.SetUniform1f("u_Opacity", opacity);
-        shader.SetUniform1f("u_Time", startTime);
         shader.SetUniform4f("u_Color", 1.0f, greenValue, 1.0f, 1.0f);
 
         va.Bind();
@@ -190,4 +220,76 @@ int main(void)
  
     glfwTerminate();
     exit(EXIT_SUCCESS);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    float cameraSpeed = 2.5 * deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    if (fov >= 1.0f && fov <= 45.0f)
+        fov -= yoffset;
+    if (fov <= 1.0f)
+        fov = 1.0f;
+    if (fov >= 45.0f)
+        fov = 45.0f;
 }
